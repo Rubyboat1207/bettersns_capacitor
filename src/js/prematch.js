4b61966e999ef8b9abc6ee5a27a6625a5a760b94
Rubyboat <rudy@rudysoliz.com>
2023-01-08 17:27:32 -0600

    prematch completed

--- src/js/prematch.js
+++ src/js/prematch.js
@@ -1,9 +1,80 @@
-
-
-
+let scouter, team, teamNumber, match, preload, noShow, cone, cube;
 addEventListener("load", () => {
-    document.getElementById("auton").addEventListener("click", () => {
-        window.location = "./auton.html";
+    scouter = document.getElementById("scouter");
+    team = document.getElementById("teamName");
+    teamNumber = document.getElementById("teamNumber");
+    match = document.getElementById("match");
+    preload = document.getElementById("preload");
+    noShow = document.getElementById("noShow");
+
+    document.getElementById("auton").addEventListener("click", nextPage);
+
+    cone = document.getElementById("cone");
+    cone.addEventListener("click", () => {
+        cone.setAttribute("src", cone.parentElement.getAttribute("select-value") == "cone" ? "assets/imgs/cone.svg" : "assets/imgs/cone_selected.svg");
+        cube.setAttribute("src", "assets/imgs/cube.svg");
     });
-})
+    cube = document.getElementById("cube");
+    cube.addEventListener("click", () => {
+        cube.setAttribute("src", cube.parentElement.getAttribute("select-value") == "cube" ? "assets/imgs/cube.svg" : "assets/imgs/cube_selected.svg");
+        cone.setAttribute("src", "assets/imgs/cone.svg");
+    });
+
+    document.getElementById("back").addEventListener("click", () => {
+        goPage("index");
+    });
+    loadFromLocalStorage();
+});
+
+function formIsValid() {
+    if (scouter.value == "") {
+        alert("Please enter your name before continuing");
+        return false;
+    }
+    if (team.value == "") {
+        alert("Please enter a team name before continuing");
+        return false;
+    }
+    if (teamNumber.value == "") {
+        alert("Please enter a team number before continuing");
+        return false;
+    }
+    if (match.value == "") {
+        alert("Please enter a match number before continuing");
+        return false;
+    }
+    if(window.localStorage.getItem("alliance") == null) {
+        alert("Please select an alliance before continuing");
+        return false;
+    }
+    return true;
+}
+
+function saveToLocalStorage() {
+    window.localStorage.setItem("scouter", scouter.value);
+    window.localStorage.setItem("teamName", team.value);
+    window.localStorage.setItem("teamNumber", teamNumber.value);
+    window.localStorage.setItem("match", match.value);
+    window.localStorage.setItem("noShow", noShow.checked);
+    window.localStorage.setItem("preload", preload.getAttribute("select-value"));
+}
+
+function loadFromLocalStorage() {
+    scouter.value = window.localStorage.getItem("scouter");
+    team.value = window.localStorage.getItem("teamName");
+    teamNumber.value = window.localStorage.getItem("teamNumber");
+    match.value = window.localStorage.getItem("match");
+    noShow.checked = window.localStorage.getItem("noShow") == "true";
+    preload.setAttribute("select-value", window.localStorage.getItem("preload"));
+    cone.setAttribute("src", preload.getAttribute("select-value") == "cone" ? "assets/imgs/cone_selected.svg" : "assets/imgs/cone.svg");
+    cube.setAttribute("src", preload.getAttribute("select-value") == "cube" ? "assets/imgs/cube_selected.svg" : "assets/imgs/cube.svg");
+    
+}
+
+function nextPage() {
+    if (formIsValid()) {
+        saveToLocalStorage();
+        goPage("auton");
+    }
+}
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,13 @@
+// apolgies for anyone who has to read this, not my best work.
+// rewriting this in react would be a good idea, but I don't have the time
+
+// lets keep the fact that this is a mess a secret, it looks nice from the outside
+
 let red;
 let blue;
 
+// This is a list of all the pages, and their orientation
+// This is used to lock the screen orientation to the correct orientation
 const pageconfigs = {
     "index": {
         orientation: "portrait",
@@ -16,7 +23,7 @@ const pageconfigs = {
     },
 }
 
-
+// This is a generic function for going to a page, and locking the screen orientation accordingly
 function goPage(page) {
     window.location = `./${page}.html`;
     window.screen.orientation.lock(pageconfigs[page].orientation);
@@ -24,7 +31,8 @@ function goPage(page) {
 
 
 
-
+// These two functions are a wrapper for the generic select function
+// So that you can include it in the HTML, instead of having to write a listener
 function selectRed() {
     select(red);
     window.localStorage.setItem("alliance", false);
@@ -35,6 +43,7 @@ function selectBlue() {
     window.localStorage.setItem("alliance", true);
 }
 
+// a generic function for selecting a tab
 function select(tab) {
     const other = tab == red ? blue : red;
 
@@ -44,6 +53,7 @@ function select(tab) {
     }
 }
 
+// lets you select alliances
 function registerTabEvents() {
     red = document.getElementById("red");
     blue = document.getElementById("blue");
@@ -52,6 +62,7 @@ function registerTabEvents() {
     blue.children[0].addEventListener("click", selectBlue)
 }
 
+// This is for the custom checkbox element
 function registerToggleEvents() {
     let checkboxes = document.getElementsByTagName("checkbox");
     for(let i = 0; i < checkboxes.length; i++) {
@@ -66,18 +77,51 @@ function registerToggleEvents() {
     }
 }
 
+// This code allows for selecting a value from a list of options
+// it then sets which option is selected in the attribute "select-value"
+function registerMultiselectEvents() {
+    let multiselects = document.getElementsByTagName("multiselect");
+    for(let i = 0; i < multiselects.length; i++) {
+        multiselects[i].addEventListener("click", (element) => {
+            // Finds the multiselect element in the parent tree
+            let multiselect = element.target;
+            while(multiselect.tagName.toLowerCase() != "multiselect") {
+                multiselect = multiselect.parentElement;
+            }
+            
+            // Nulls the select-value attribute if the same option is selected twice
+            const old_selected = multiselect.getAttribute("select-value");
+            const new_selected = element.target.getAttribute("select-name");
+            if(old_selected == new_selected) {
+                multiselect.setAttribute("select-value", "");
+            }else {
+                multiselect.setAttribute("select-value", new_selected);
+            }
+        });
+    }
+}
+
+// Sets up the page, and should run before all other load listeners
 addEventListener("load", () => {
+    // -- Alliance Selection --
     if(document.getElementById("red") && document.getElementById("blue")) {
         registerTabEvents();
+        if(window.localStorage.getItem("alliance") == "true") {
+            select(blue);
+        }else {
+            select(red);
+        }
     }
-    registerToggleEvents();
-
-    if(document.getElementById("alliance-display")) {
-        let display = document.getElementById("alliance-display");
+    const display = document.getElementById("alliance-display");
+    if(display) {
         if(window.localStorage.getItem("alliance") == "true") {
             display.classList.add("d_blue");
         }else {
             display.classList.add("d_red");
         }
     }
+    // -- Toggle --
+    registerToggleEvents();
+    // -- Multiselect --
+    registerMultiselectEvents();
 })
