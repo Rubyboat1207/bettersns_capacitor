c25ec3810942630b577d1cfc8225c37f8d7079ad
Rubyboat <rudy@rudysoliz.com>
2023-01-08 23:17:00 -0600

    Auton Mostly Finished

--- src/js/field.js
+++ src/js/field.js
@@ -1,61 +1,117 @@
+const icons = document.getElementsByClassName("icon");
+let floor_icons;
+
 const alliance = window.localStorage.getItem('alliance');
-let icons_location = []; 
+let icons_location = [];
+
+function setGridLocation(loc, value) {
+    if(loc.y != 2) {
+        let i = loc.x * 2 + loc.y;
+        if(value == true) {
+            icons[i].classList.add("selected");
+        }
+        else {
+            icons[i].classList.remove("selected");
+        }
+        icons_location[i].positive = value;
+        return;
+    }
+
+    const icon = floor_icons[loc.x];
+    let child = icon.children[0];
+    icons_location[18 + loc.x].type = value;
+
+    if(child == null) {
+        if(value == null) {
+            return;
+        }
+        child = document.createElement("img");
+        icon.appendChild(child);
+    }
+
+    child.classList.remove("cube");
+    child.classList.remove("cone");
+
+    if(value == null) {
+        child.remove();
+        return;
+    }
+
+    child.setAttribute("src", `assets/imgs/${value}.svg`);
+    child.classList.add(`${value}`);
+}
+
+function registerElevatedIcons() {
+    floor_icons = document.getElementById('floor').children;
+    for(let i = 0; i < icons.length; i++) {
+        const loc = {
+            x: Math.floor(i / 2),
+            y: i % 2,
+            positive: false
+        };
+        icons_location.push(loc)
+
+        icons[i].addEventListener("click", () => {
+            setGridLocation(loc, !icons_location[i].positive);
+            if(!icons_location[i].positive) {
+                return;
+            }
+            if(loc.x >= 3 && loc.x <= 5) {
+                const place = document.getElementById("community-place");
+                if(!place.classList.contains("checked")) {
+                    document.getElementById("community-place").click();
+                }
+            }
+        });
+    }
+}
+
+function registerFloorIcons() {
+    const floor = document.getElementById("floor");
+    for(let i = 0; i < floor.children.length; i++) {
+        const loc = {
+            x: i,
+            y: 2,
+            type: null
+        };
+        let space = floor.children[i];
+        space.addEventListener("click", () => {
+            const child = space.children[0];
+            if(loc.x >= 3 && loc.x <= 5) {
+                const place = document.getElementById("community-place");
+                if(!place.classList.contains("checked")) {
+                    document.getElementById("community-place").click();
+                }
+            }
+
+            if(child == null) {
+                setGridLocation(loc, "cube");
+                return;
+            }
+
+            if(child.classList.contains("cube")) {
+                setGridLocation(loc, "cone");
+                return;
+            }
+
+            setGridLocation(loc, null);
+        });
+        icons_location.push(loc);
+    }
+}
+
 addEventListener('load', function() {
+    registerElevatedIcons();
+    registerFloorIcons();
     if(alliance) {
         const side_grids = document.getElementsByClassName("side-grids");
         for(let grid of side_grids) {
             grid.classList.add(alliance == "true" ? "blue" : "red");
         }
     }
-    const cubes = document.getElementsByClassName("cube");
-    const cones = document.getElementsByClassName("cone");
-    const icons = document.getElementsByClassName("icon");
-    for(let i = 0; i < icons.length; i++) {
-        let icon = icons[i];
-        icons_location.push({
-            x: Math.floor(i / 2),
-            y: i % 2,
-            positive: false
-        })
-        icon.setAttribute("index", i);
-        //console.log(icons_location[i].x, icons_location[i].y)
-        icon.addEventListener("click", () => {
-            icon.classList.toggle("selected");
-            icons_location[i].positive = !icons_location[i].positive;
-            //console.log(`Icon ${i} is now ${icons_location[i].selected ? "selected" : "unselected"}`);
-        });
-    }
-    const floor = document.getElementById("floor");
-    for(let i = 0; i < floor.children.length; i++) {
-        let space = floor.children[i];
-        space.addEventListener("click", () => {
-            const index = space.getAttribute("index");
-            const child = space.children[0];
-            if(child == null) {
-                let child = document.createElement("img");
-                child.setAttribute("src", "assets/imgs/cube.svg");
-                child.classList.add("cube");
-                space.appendChild(child);
-                icons_location[index].type = "cube";
-            }
-            else if(child.classList.contains("cube")) {
-                child.setAttribute("src", "assets/imgs/cone.svg");
-                child.classList.remove("cube");
-                child.classList.add("cone");
-                icons_location[index].type = "cone";
-            }
-            else {
-                child.remove();
-                icons_location[index].type = null;
-            }
-            //console.log(`Space ${index} is now a ${icons_location[index].type}`);
-        });
-        icons_location.push({
-            x: i,
-            y: 2,
-            type: null
-        });
-        space.setAttribute("index", icons_location.length - 1);
-    }
+    this.document.getElementById("charge").addEventListener("click", (e) => {
+        let charged = e.target.getAttribute("src") == "assets/imgs/successful_charge.png";
+        e.target.setAttribute("src", charged ? "assets/imgs/unsuccessful_charge.svg" : "assets/imgs/successful_charge.png");
+    });
     console.log(icons_location);
 });
