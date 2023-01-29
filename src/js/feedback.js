01827c26a2a96ae7a8fd369d9d8a2f5083e1d873
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2023-01-29 17:04:17 -0600

    commit for laptop

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,3 +1,10 @@
 addEventListener("load", () => {
     document.getElementById("back").addEventListener("click", () => { goPage("start") })
 })
+
+function formIsValid() {
+    if (scouter.value == "") {
+        alert("Please enter your name before sending feedback");
+        return false;
+    }
+}
