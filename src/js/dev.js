c255d0a25df91a676158901a33c1c581fed15ac5
Rubyboat1207 <rudy@rudysoliz.com>
2023-03-18 11:17:42 -0500

    preload type

--- src/js/dev.js
+++ src/js/dev.js
@@ -6,11 +6,13 @@ document.getElementById('back').addEventListener('click', () => {
 
 document.getElementById('updateCache').addEventListener('click', () => {
     localStorage.setItem('requestCache', JSON.parse(document.getElementById('localStorage').value))
+
     updateCacheWithNewValues()
 })
 
 function updateCacheWithNewValues() {
     document.getElementById('localStorage').value = JSON.stringify(JSON.parse(localStorage.getItem('requestCache')), null, '  ')
+    console.log(JSON.stringify(JSON.parse(localStorage.getItem('requestCache'))))
 }
 updateCacheWithNewValues();
 
